285575eddb442c99c1bdd13c6c4b98555810dfae
imndx <imndxx@gmail.com>
2020-10-13 15:07:47 +0800

    朋友圈内消息列表显示feed摘要

--- src/main/java/cn/wildfire/chat/moment/FeedMessageViewHolder.java
+++ src/main/java/cn/wildfire/chat/moment/FeedMessageViewHolder.java
@@ -11,16 +11,19 @@ import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import cn.wildfire.chat.kit.GlideApp;
+import cn.wildfire.chat.kit.R;
 import cn.wildfire.chat.kit.third.utils.TimeUtils;
 import cn.wildfire.chat.moment.third.interfaces.OnFeedUserClickListener;
 import cn.wildfire.chat.moment.third.span.VerticalImageSpan;
-import cn.wildfire.chat.kit.R;
 import cn.wildfirechat.message.Message;
 import cn.wildfirechat.message.MessageContent;
 import cn.wildfirechat.model.UserInfo;
 import cn.wildfirechat.moment.FeedCommentType;
+import cn.wildfirechat.moment.FeedContentType;
+import cn.wildfirechat.moment.MomentClient;
 import cn.wildfirechat.moment.message.FeedCommentMessageContent;
 import cn.wildfirechat.moment.message.FeedMessageContent;
+import cn.wildfirechat.moment.model.Feed;
 import cn.wildfirechat.remote.ChatManager;
 
 public class FeedMessageViewHolder extends RecyclerView.ViewHolder {
@@ -59,23 +62,49 @@ public class FeedMessageViewHolder extends RecyclerView.ViewHolder {
     public void onBind(Message message) {
         MessageContent messageContent = message.content;
         String sender = "";
+        long feedId = 0;
         if (messageContent instanceof FeedMessageContent) {
             FeedMessageContent feedMessageContent = (FeedMessageContent) messageContent;
             sender = feedMessageContent.getSender();
             contentTextView.setText("提到了我");
+            feedId = feedMessageContent.getFeedId();
         } else if (messageContent instanceof FeedCommentMessageContent) {
             FeedCommentMessageContent feedCommentMessageContent = (FeedCommentMessageContent) messageContent;
             sender = feedCommentMessageContent.getSender();
+            feedId = feedCommentMessageContent.getFeedId();
             if (feedCommentMessageContent.getCommentType() == FeedCommentType.Comment_Thumbup_Type) {
                 SpannableStringBuilder builder = new SpannableStringBuilder();
                 builder.append("赞");
                 builder.setSpan(new VerticalImageSpan(ContextCompat.getDrawable(itemView.getContext(), R.drawable.heart_drawable_blue)),
-                        0, 1, Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+                    0, 1, Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
                 contentTextView.setText(builder);
             } else {
                 contentTextView.setText(feedCommentMessageContent.getText());
             }
         }
+
+        MomentClient.getInstance().getFeed(feedId, new MomentClient.GetFeedCallback() {
+            @Override
+            public void onSuccess(Feed feed) {
+                if (feed.type == FeedContentType.Content_Text_Type) {
+                    descTextView.setVisibility(View.VISIBLE);
+                    descTextView.setText(feed.text == null ? "" : feed.text);
+                } else {
+                    if (feed.medias != null && !feed.medias.isEmpty()) {
+                        GlideApp.with(descImageView).load(feed.medias.get(0).thumbUrl).into(descImageView);
+                    }
+                    descTextView.setText(feed.text == null ? "" : feed.text);
+                }
+
+            }
+
+            @Override
+            public void onFailure(int errorCode) {
+
+            }
+        });
+//        descTextView.setText(feedMessageContent.getText() == null ? "" : feedMessageContent.getText());
+
         UserInfo userInfo = ChatManager.Instance().getUserInfo(sender, false);
         userId = userInfo.uid;
 
